20b6c47454a9fc8fa5622033622298709d25af00
valera2872 <barbarov28@protonmail.com>
2026-08-08 10:22:17 +0300

    Mark private cabinet as noindex

--- web/app/cabinet/layout.tsx
+++ web/app/cabinet/layout.tsx
@@ -1,4 +1,13 @@
+import type { Metadata } from "next";
 import "./cabinet.css";
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+  },
+};
 
 export default function CabinetLayout({
   children,
